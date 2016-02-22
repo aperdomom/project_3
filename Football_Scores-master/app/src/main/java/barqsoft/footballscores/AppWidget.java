77a971cc6c8a2d393d2df36c4c44ec247c3d8142
Alexandra Perdomo <aperdomo@mahisoft.com>
2016-02-22 17:34:26 -0600

    Fixing widt=get UI

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/AppWidget.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/AppWidget.java
@@ -5,7 +5,13 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
 import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
@@ -21,10 +27,37 @@ public class AppWidget
         CharSequence widgetText = AppWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
-        //views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        Cursor data = null;
+        Uri todayUri = DatabaseContract.scores_table.buildScoreWithDate();
+        String strDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date(System.currentTimeMillis()));
+        data = context.getContentResolver().query(todayUri,
+                                                 null,
+                                                 null,
+                                                 new String[]{strDate},
+                                                 DatabaseContract.scores_table.DATE_COL + " ASC");
+        String result = "";
+        assert data != null;
+        if (data.moveToFirst()) {
+            views.setTextViewText(R.id.tite_text,
+                                  "Today "+
+                                  data.getString(data.getColumnIndex(DatabaseContract.scores_table.TIME_COL)));
+            views.setTextViewText(R.id.home_name_text,
+                                  data.getString(data.getColumnIndex(DatabaseContract.scores_table.HOME_COL)));
+            views.setTextViewText(R.id.goals_text,
+                                  data.getString(data.getColumnIndex(DatabaseContract.scores_table.HOME_GOALS_COL))+
+                                    "-" + data.getString(data.getColumnIndex(DatabaseContract.scores_table.AWAY_GOALS_COL))
+            );
+            views.setTextViewText(R.id.away_name_text,
+                                  data.getString(data.getColumnIndex(DatabaseContract.scores_table.AWAY_COL)));
+            result +=  data.getString(data.getColumnIndex(DatabaseContract.scores_table.HOME_COL));
+            Log.e("widget", "Result: "+result);
+        }
+        data.close();
+
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
-        views.setOnClickPendingIntent(R.id.appwidget_text,pendingIntent);
+        views.setOnClickPendingIntent(R.id.widget_linear, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
