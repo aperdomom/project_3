516fb8dc100b88203f4d965c4a8c357fd60c9202
Alexandra Perdomo <aperdomo@mahisoft.com>
2016-02-22 21:00:05 -0600

    Adding string labels - Fixing permission

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/utils/Utilies.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/utils/Utilies.java
@@ -1,5 +1,7 @@
 package barqsoft.footballscores.utils;
 
+import android.content.Context;
+
 import barqsoft.footballscores.R;
 
 /**
@@ -12,46 +14,46 @@ public class Utilies
     public static final int CHAMPIONS_LEAGUE = 362;
     public static final int PRIMERA_DIVISION = 358;
     public static final int BUNDESLIGA = 351;
-    public static String getLeague(int league_num)
+    public static int getLeague(int league_num)
     {
         switch (league_num)
         {
-            case SERIE_A : return "Seria A";
-            case PREMIER_LEGAUE : return "Premier League";
-            case CHAMPIONS_LEAGUE : return "UEFA Champions League";
-            case PRIMERA_DIVISION : return "Primera Division";
-            case BUNDESLIGA : return "Bundesliga";
-            default: return "Not known League Please report";
+            case SERIE_A : return R.string.label_seria_a;
+            case PREMIER_LEGAUE : return R.string.label_premier_league;
+            case CHAMPIONS_LEAGUE : return R.string.label_uefa;
+            case PRIMERA_DIVISION : return R.string.label_primera;
+            case BUNDESLIGA : return R.string.label_bundes;
+            default: return R.string.label_not_know;
         }
     }
-    public static String getMatchDay(int match_day,int league_num)
+    public static String getMatchDay(Context context,int match_day,int league_num)
     {
         if(league_num == CHAMPIONS_LEAGUE)
         {
             if (match_day <= 6)
             {
-                return "Group Stages, Matchday : 6";
+                return context.getString(R.string.label_group_stages);
             }
             else if(match_day == 7 || match_day == 8)
             {
-                return "First Knockout round";
+                return context.getString(R.string.label_first_round);
             }
             else if(match_day == 9 || match_day == 10)
             {
-                return "QuarterFinal";
+                return context.getString(R.string.label_quarter);
             }
             else if(match_day == 11 || match_day == 12)
             {
-                return "SemiFinal";
+                return context.getString(R.string.label_semi);
             }
             else
             {
-                return "Final";
+                return context.getString(R.string.label_final);
             }
         }
         else
         {
-            return "Matchday : " + String.valueOf(match_day);
+            return context.getString(R.string.label_match_day) +" "+ String.valueOf(match_day);
         }
     }
 
